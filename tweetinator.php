31a55825901cf88d6517bbb756a3217ee2bbc6fb
Jonas Felix <jonas.felix@felixideas.ch>
2016-09-30 15:15:06 +0200

    migrates to psr4, namespaces

--- tweetinator.php
+++ tweetinator.php
@@ -17,8 +17,6 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-namespace tweetinator;
+require('vendor/autoload.php');
 
-require_once('tweetinatorBoot.php');
-
-$tweetinatorBoot = new tweetinatorBoot();
+$TweetIt = new \Wingsuitist\Tweetinator\TweetIt();
